d3a6430e8f60f0195195cb032e7f3e4ca9b4c9e6
RoecDesenvolvimentoWeb <roec.roecdesenvolvimentoweb@gmail.com>
2024-07-03 15:12:06 -0300

    feat:create a test route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import csvRouter from "./routes/csv.routes.js";
 
 dotenv.config();
 
@@ -18,3 +19,5 @@ const app = express();
 app.listen("5000", () => {
   console.log("Server is running on port 5000");
 });
+
+app.use("/api", csvRouter);
