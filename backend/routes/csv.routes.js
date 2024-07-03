9a718e21671b62d6937c0e1d2767b3554af1e528
RoecDesenvolvimentoWeb <roec.roecdesenvolvimentoweb@gmail.com>
2024-07-03 16:05:27 -0300

    feat: send a csv data file trought multipart form

--- backend/routes/csv.routes.js
+++ backend/routes/csv.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import multer from "multer";
-
 import { uploadCsvFile } from "../controllers/csv.controller.js";
+
+const upload = multer({ dest: "upload/" });
+
 const router = express.Router();
 
-router.post("/upload", uploadCsvFile);
+router.post("/upload", upload.single("file"), uploadCsvFile);
 
 export default router;
