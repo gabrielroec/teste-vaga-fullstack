926312fe5e9dc9404cf528404ad7481c36a1f399
RoecDesenvolvimentoWeb <roec.roecdesenvolvimentoweb@gmail.com>
2024-07-03 18:42:43 -0300

    try to parse csv files

--- backend/controllers/csv.controller.js
+++ backend/controllers/csv.controller.js
@@ -1,36 +1,50 @@
 import csvFileData from "../models/csv.model.js";
+import fs from "fs";
+import csv from "csv-parser";
+import path from "path";
+
+const parseCsv = (csvFilePath) => {
+  const results = [];
+  return new Promise((res, rej) => {
+    fs.createReadStream(csvFilePath)
+      .pipe(csv())
+      .on("data", (data) => {
+        results.push(data);
+      })
+      .on("end", () => {
+        res(results);
+      })
+      .on("error", (error) => {
+        rej(error);
+      });
+  });
+};
+
+const formateCsvFile = async (csvFilePath) => {
+  const csvData = await parseCsv(csvFilePath);
+  const formattedCsvFiles = [];
+
+  for (const item of csvData) {
+    const formattedCsvFile = {
+      vlTotal: item.vlTotal,
+    };
+    formattedCsvFiles.push(formattedCsvFile);
+  }
+
+  console.log(formattedCsvFiles);
+
+  await csvFileData.insertMany(formattedCsvFiles);
+  return formattedCsvFiles;
+};
 
 export const uploadCsvFile = async (req, res) => {
-  const {
-    nrCpfCnpj,
-    vlTotal,
-    vlPresta,
-    vlMora,
-    vlMovimento,
-    vlPago,
-    qtPresta,
-    qtPrestacoes,
-    dtContrato,
-    dtVctPre,
-  } = req.body;
-
   try {
-    const newRecivedData = new csvFileData({
-      nrCpfCnpj,
-      vlTotal,
-      vlPresta,
-      vlMora,
-      vlMovimento,
-      vlPago,
-      qtPresta,
-      qtPrestacoes,
-      dtContrato,
-      dtVctPre,
-    });
-
-    const savedData = await newRecivedData.save();
-    res.status(201).json(savedData);
+    console.log("START");
+    const csvFilePath = path.join(__dirname, "..", "upload", req.file.filename);
+    const data = await formateCsvFile(csvFilePath);
+    res.status(200).json(data);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    console.error(error);
+    res.status(500).json({ message: "Error processing csv file" });
   }
 };
