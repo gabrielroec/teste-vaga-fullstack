3019e7da123ec8420c9a6c7317ad2d9047da528b
Gabriel Roecc <gabrielroecc@gmail.com>
2024-07-06 18:15:40 -0300

    fix:adjust future connects troubles for recruiters

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import csvRouter from "./routes/csv.routes.js";
 import cors from "cors";
+
+// Por motivos de agilidade para avaliar o teste, decidi deixar o .env descomentado e colocar a key do MongoDb direto no código
+// E também liberei o "Access from anywhere no MongoDb para os avaliadores não terem problema de conexão ao banco de dados"
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(
+    "mongodb+srv://roecwebdev:asuh1@kronoos-test-cluster.kvrjc4x.mongodb.net/?retryWrites=true&w=majority&appName=KRONOOS-TEST-CLUSTER"
+  )
   .then(() => {
     console.log("MongoDb is connected");
   })
